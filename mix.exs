4f30c42c314898dff39a40a6ba36368df32c9fe8
Marc Neudert <marc.neudert@gmail.com>
2017-03-14 20:25:39 +0100

    ws

--- mix.exs
+++ mix.exs
@@ -30,9 +30,8 @@ defmodule Geolix.Adapter.MMDB2Caching.Mixfile do
   end
 
   defp deps do
-    [ { :ex_doc, ">= 0.0.0", only: :dev },
-
-      { :excoveralls, "~> 0.6", only: :test } ]
+    [ { :ex_doc,      ">= 0.0.0", only: :dev },
+      { :excoveralls, "~> 0.6",   only: :test } ]
   end
 
   defp docs do
