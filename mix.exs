fe3bec1246bd38b9d2385a92c202b72df32d1084
Marc Neudert <marc.neudert@gmail.com>
2017-09-23 17:15:27 +0200

    raises elixir version requirement to "~> 1.3"

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Geolix.Adapter.MMDB2Caching.Mixfile do
     [ app:     :geolix_adapter_mmdb2_caching,
       name:    "Geolix Adapter: MMDB2 (Caching)",
       version: "0.1.0-dev",
-      elixir:  "~> 1.2",
+      elixir:  "~> 1.3",
       deps:    deps(),
 
       build_embedded:  Mix.env == :prod,
