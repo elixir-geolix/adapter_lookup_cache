550368c01a3fa235a1e47020f9b7c3c4d8cd64d3
Marc Neudert <marc.neudert@gmail.com>
2017-10-18 19:08:57 +0200

    performs initial "mix format"

--- mix.exs
+++ mix.exs
@@ -4,47 +4,52 @@ defmodule Geolix.Adapter.MMDB2Caching.Mixfile do
   @url_github "https://github.com/elixir-geolix/adapter_mmdb2_caching"
 
   def project do
-    [ app:     :geolix_adapter_mmdb2_caching,
-      name:    "Geolix Adapter: MMDB2 (Caching)",
+    [
+      app: :geolix_adapter_mmdb2_caching,
+      name: "Geolix Adapter: MMDB2 (Caching)",
       version: "0.1.0-dev",
-      elixir:  "~> 1.3",
-      deps:    deps(),
-
-      build_embedded:  Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-
+      elixir: "~> 1.3",
+      deps: deps(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       preferred_cli_env: [
-        coveralls:          :test,
-        'coveralls.detail': :test,
-        'coveralls.travis': :test
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.travis": :test
       ],
-
-      description:   "MMDB2 adapter for Geolix with integrated caching capabilities",
-      docs:          docs(),
-      package:       package(),
-      test_coverage: [ tool: ExCoveralls ] ]
+      description: "MMDB2 adapter for Geolix with integrated caching capabilities",
+      docs: docs(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   def application do
-    [ applications: [ :logger ]]
+    [applications: [:logger]]
   end
 
   defp deps do
-    [ { :ex_doc,      ">= 0.0.0", only: :dev },
-      { :excoveralls, "~> 0.7",   only: :test } ]
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
   end
 
   defp docs do
-    [ extras:     [ "CHANGELOG.md", "README.md" ],
-      main:       "readme",
+    [
+      extras: ["CHANGELOG.md", "README.md"],
+      main: "readme",
       source_ref: "master",
-      source_url: @url_github ]
+      source_url: @url_github
+    ]
   end
 
   defp package do
-    %{ files:       [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
-       licenses:    [ "Apache 2.0" ],
-       links:       %{ "GitHub" => @url_github },
-       maintainers: [ "Marc Neudert" ] }
+    %{
+      files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => @url_github},
+      maintainers: ["Marc Neudert"]
+    }
   end
 end
