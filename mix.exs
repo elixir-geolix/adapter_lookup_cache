78af3ae4bbdc31e2ec8352ec01ba401a2d7bebc3
Marc Neudert <marc.neudert@gmail.com>
2019-06-17 18:57:22 +0200

    Setup code quality checks

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule Geolix.Adapter.LookupCache.MixProject do
       elixir: "~> 1.5",
       deps: deps(),
       description: "Lookup cache adapter for Geolix",
+      dialyzer: dialyzer(),
       docs: docs(),
       package: package(),
       preferred_cli_env: [
@@ -28,8 +29,21 @@ defmodule Geolix.Adapter.LookupCache.MixProject do
 
   defp deps do
     [
+      {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.11", only: :test, runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      flags: [
+        :error_handling,
+        :race_conditions,
+        :underspecs,
+        :unmatched_returns
+      ]
     ]
   end
 
