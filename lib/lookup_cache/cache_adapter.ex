d2761d844714071652e678a05870282f4da691ca
Marc Neudert <marc.neudert@gmail.com>
2019-07-15 18:55:57 +0200

    Stub behaviour for cache adapters

---
+++ lib/lookup_cache/cache_adapter.ex
@@ -0,0 +1,38 @@
+defmodule Geolix.Adapter.LookupCache.CacheAdapter do
+  @moduledoc """
+  Behaviour a cache adapter is expected to follow.
+  """
+
+  @optional_callbacks [
+    cache_workers: 1
+  ]
+
+  @doc """
+  Returns the children to be supervised for this cache.
+
+  If no automatic supervision should take place or it is intended to use an
+  adapter specific supervisor (e.g. using the application config) this callback
+  should be either unimplemented or return an empty list.
+  """
+  @callback cache_workers(database :: map) :: list
+
+  @doc """
+  Perform a cache lookup and return the result.
+
+  The result value is treated as follows:
+
+  - `{:ok, nil}` - No cached value found, will be updated after lookup
+  - `{:ok, map}` - Cached value found, will be returned directly to caller
+  - `{:error, term}` - Cache error, lookup will not be saved to cache
+  """
+  @callback get(ip :: :inet.ip_address(), opts :: Keyword.t(), database :: map) ::
+              {:ok, term} | {:error, term}
+
+  @doc """
+  Store a lookup result in the cache.
+
+  The adapter should (for now) always return `:ok` and handle errors internally.
+  """
+  @callback put(ip :: :inet.ip_address(), opts :: Keyword.t(), database :: map, result :: map) ::
+              :ok
+end
