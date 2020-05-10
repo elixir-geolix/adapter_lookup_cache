fc26655a77f2e5a4d46c4ce4093b8859796c3be5
Marc Neudert <marc.neudert@gmail.com>
2020-05-10 12:27:20 +0200

    Bump :geolix to "~> 1.1"

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Geolix.Adapter.LookupCache.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12.0", only: :test, runtime: false},
-      {:geolix, github: "elixir-geolix/geolix", ref: "05592951c028b458e3801f56a1adb66b8f320ce1"}
+      {:geolix, "~> 1.1"}
     ]
   end
 
