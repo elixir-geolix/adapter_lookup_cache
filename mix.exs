a018fcb633e51217189096565d67f25993fb1bd1
Marc Neudert <marc.neudert@gmail.com>
2020-05-10 12:23:13 +0200

    Loosen :dialyxir requirement

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Geolix.Adapter.LookupCache.MixProject do
       {:cachex, "~> 3.0", optional: true},
       {:con_cache, "~> 0.14.0", optional: true},
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test, runtime: false},
       {:geolix, github: "elixir-geolix/geolix", ref: "05592951c028b458e3801f56a1adb66b8f320ce1"}
