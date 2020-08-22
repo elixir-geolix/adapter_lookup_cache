148d85310a7b582454a58d2691eb6081f79ea840
Marc Neudert <marc.neudert@gmail.com>
2020-08-22 13:34:23 +0200

    Remove deprecated hex :maintainers metadata

--- mix.exs
+++ mix.exs
@@ -63,8 +63,7 @@ defmodule Geolix.Adapter.LookupCache.MixProject do
     %{
       files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => @url_github},
-      maintainers: ["Marc Neudert"]
+      links: %{"GitHub" => @url_github}
     }
   end
 end
